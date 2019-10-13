0b47cec83289a8e9ed7da236fa2064e70ddd7d08
John Heinrich Austria <heinrichaustria@gmail.com>
2019-10-14 00:46:45 +0800

    Create Project model

---
+++ models/Project.js
@@ -0,0 +1,49 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 300,
+        unique: true
+    },
+    urls: {
+        feat_img: {
+            type: String
+        },
+        docs: {
+            type: String
+        },
+        demo: {
+            type: String
+        }
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+function validate(project) {
+    const schema = {
+        name: Joi.string().required().max(50),
+        description: Joi.string().required().max(300),
+        creator: Joi.string().required()
+    };
+
+    return Joi.validate(project, schema);
+}
+
+module.exports = {
+    Project,
+    validate
+};
